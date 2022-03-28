cd13ee6f952d4d86c7a30a944e413dd08dfa4a3c
qianruzh <qianruzzz@gmail.com>
2022-03-28 13:29:36 -0400

    Integrated mdb/ Added tab & accordion

--- src/index.js
+++ src/index.js
@@ -424,4 +424,24 @@ const firebaseApp = initializeApp(getFirebaseConfig());
 getPerformance();
 initFirebaseAuth();
 loadMessages();
+
+//change tab
+document.addEventListener('DOMContentLoaded', function(){
+  const tabs = document.getElementsByClassName('mdl-tabs__tab');
+  for(let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', tabSwitch, false);
+  }
+
+  // タブをクリックすると実行する関数
+  function tabSwitch(){
+    // タブのclassの値を変更
+    document.getElementsByClassName('is-active')[0].classList.remove('is-active');
+    this.classList.add('is-active');
+    // コンテンツのclassの値を変更
+    document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+    const arrayTabs = Array.prototype.slice.call(tabs);
+    const index = arrayTabs.indexOf(this);
+    document.getElementsByClassName('mdl-tabs__panel')[index].classList.add('is-show');
+  };
+}, false);
  
